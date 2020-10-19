93a827c33d0c1c8d1713ac8518eb856190a95007
Jeremy Brammer <jbrammer03@gmail.com>
2020-10-18 23:09:48 -0500

    Installed Bootstrap.
    Installed laravel ui bootstrap scaffolding to make UI look better.

--- resources/views/imageuploads/index.blade.php
+++ resources/views/imageuploads/index.blade.php
@@ -1,7 +1,17 @@
-<div class="card" style="width: 18rem;">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">{{ $someData }}</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-</div>
+<html>
+    <head>
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+    <body>
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">{{ $someData }}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </body>
+</html>
