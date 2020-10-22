6be7373715158221b38bfbb992f5078c52693146
Jeremy Brammer <jbrammer03@gmail.com>
2020-10-22 18:47:52 -0500

    Added dependency (flysystem-aws-s3-v3) to my package, and that updated the main composer.lock file.

--- resources/views/imageuploads/index.blade.php
+++ resources/views/imageuploads/index.blade.php
@@ -6,6 +6,8 @@
             <h5 class="card-title">Upload an Image</h5>
             <p class="card-text">Upload an image to s3. {{ $testData }}</p>
 
+            {{-- <img src="{{ Storage::url('images/aD4cMvZoc7rlW39CbSALKCop0bmitcjuSO5M8YIk.png') }}" /> --}}
+
             <form id="image-upload" class="image-upload-form" action="{{ route("imageuploads.store") }}" method="post" enctype="multipart/form-data">
                 @csrf
                 <input type="file" id="image-upload-field" name="image-upload-field" accept="image/*"/>
